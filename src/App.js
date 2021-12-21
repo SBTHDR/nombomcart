714e63bab6f40c0ce7313a2bd31346906258cd50
SBTHDR <subrata.halder13d@gmail.com>
2021-12-21 20:49:55 +0600

    Router Config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Navigation from './components/Navigation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <>
+            <Router>
+                <Navigation />
+                <Switch>
+                    <Route path="/" component={Home} exact></Route>
+                    <Route path="/about" component={About}></Route>
+                </Switch>
+            </Router>
+        </>
+    )
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+        </>
+    )
+}
+
+export default Navigation;
