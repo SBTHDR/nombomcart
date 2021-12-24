ac754ab200b33ac4a8ded261206af668f1adb3d8
SBTHDR <subrata.halder13d@gmail.com>
2021-12-24 22:59:35 +0600

    Update cart items

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,38 +1,66 @@
+import { useContext, useEffect, useState } from 'react';
+import { CartContext } from '../CartContext';
+
 const Cart = () => {
+
+    const [products, setProducts] = useState([]);
+    const { cart, setCart } = useContext(CartContext);
+
+    useEffect(() => {
+        if (!cart.items) {
+            return;
+        }
+        fetch('/api/products/cart-items', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ ids: Object.keys(cart.items) })
+        }).then(res => res.json())
+        .then(products => {
+            setProducts(products);
+        })
+    }, [cart]);
+
+    const getQty = (productId) => {
+        return cart.items[ productId ];
+    }
+
+    const increment = (productId) => {
+        const existingQty =  cart.items[ productId ];
+        const _cart =  {...cart};
+        _cart.items[productId] = existingQty + 1;
+        _cart.totalItems += 1;
+        setCart(_cart);
+    }
+
     return (
         <div className="container mx-auto w-full lg:w-1/2 pb-24">
             <h1 className="text-3xl text-center">Shopping Cart</h1>
-            <ul className="">
-                <li className="">
-                    <div className="flex items-center justify-between mt-5">
-                        <div className="flex items-center">
-                            <img className="w-20 bg-orange-400 p-1 mr-2" src="/images/pizza.jpeg" alt="" />
-                            <span className="text-lg font-bold">Chicken BBQ Pizza</span>
-                        </div>
-                        <div className="flex items-center">
-                            <button className="bg-orange-400 px-4 py-2 rounded-full leading-none">-</button>
-                            <b className="px-2">2</b>
-                            <button className="bg-orange-400 px-4 py-2 rounded-full leading-none">+</button>
-                        </div>
-                        <span className="text-lg text-green-500 font-bold">BDT. 700</span>
-                        <button className="bg-red-500 px-4 py-2 rounded-full leading-none text-white">Delete</button>
-                    </div>
-                </li>
-                <li className="">
-                    <div className="flex items-center justify-between mt-5">
-                        <div className="flex items-center">
-                            <img className="w-20 bg-orange-400 p-1 mr-2" src="/images/pizza.jpeg" alt="" />
-                            <span className="text-lg font-bold">Chicken BBQ Pizza</span>
-                        </div>
-                        <div className="flex items-center">
-                            <button className="bg-orange-400 px-4 py-2 rounded-full leading-none">-</button>
-                            <b className="px-2">2</b>
-                            <button className="bg-orange-400 px-4 py-2 rounded-full leading-none">+</button>
-                        </div>
-                        <span className="text-lg text-green-500 font-bold">BDT. 700</span>
-                        <button className="bg-red-500 px-4 py-2 rounded-full leading-none text-white">Delete</button>
-                    </div>
-                </li>
+            <ul>
+
+                {
+                    products.map(product => {
+                        return (
+                            <li className="">
+                                <div className="flex items-center justify-between mt-5">
+                                    <div className="flex items-center">
+                                        <img className="w-20 bg-orange-400 p-1 mr-2" src={ product.image } alt="" />
+                                        <span className="text-lg font-bold">{ product.name }</span>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <button className="bg-orange-400 px-4 py-2 rounded-full leading-none">-</button>
+                                        <b className="px-2">{ getQty(product._id) }</b>
+                                        <button onClick={ () => { increment(product._id) } } className="bg-orange-400 px-4 py-2 rounded-full leading-none">+</button>
+                                    </div>
+                                    <span className="text-lg text-green-500 font-bold">BDT. { product.price }</span>
+                                    <button className="bg-red-500 px-4 py-2 rounded-full leading-none text-white">Delete</button>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
+
             </ul>
             <hr className="mt-7 text-gray-200" />
             <div className="text-lg mt-2 font-bold text-right">
